a783ff245b9bcb3bd1eadf099b7dc38b4bba41d6
Tldkt <spooning.Tldkt@gmail.com>
2022-05-08 16:09:22 +0900

    add: 게시글 리스트 작성

--- board/src/main/java/com/study/board/controller/Boardcontroller.java
+++ board/src/main/java/com/study/board/controller/Boardcontroller.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +34,12 @@ public class Boardcontroller {
         boardService.write(board);
         return "";
     }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return"boardlist";
+    }//name 있는 거로 선택하는지 몰랐고, 아직 뭐가 객체고 뭐가 메소든지 좀 헷갈린다...  글고 객체 뒤에 () 안붙여서 틀렸다
 }
 
 
